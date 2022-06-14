e327912f9ce81869b2e22c2f2dc1b9199674599a
ReGuardian <danilasuhoparov@yandex.ru>
2022-06-14 18:28:55 +0300

    Create lab_4.py

---
+++ Python/lab_4.py
@@ -0,0 +1,9 @@
+print('Введите 3 названия цветов')
+
+a = str(input())
+b = str(input())
+c = str(input())
+
+print('Все слова в нижнем регистре: ', a.lower(),' ',b.lower(),' ',c.lower())
+print('Все слова в верхнем регистре: ', a.upper(),' ',b.upper(),' ',c.upper())
+print('Все слова с большой первой буквой: ', a.title(),' ',b.title(),' ',c.title())
